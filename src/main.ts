920f79e8742352fbc74b8e9fcfd049d2681eb6b0
Gabriel melchert <gfmelchert@gmail.com>
2025-07-17 21:50:49 -0300

    feat: habilita CORS com configurações específicas no bootstrap da aplicação

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+
+    app.enableCors({
+        origin: ['http://localhost:5173'],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    });
+
+    await app.listen(process.env.PORT);
 }
 bootstrap();
