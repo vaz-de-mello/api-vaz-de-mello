5f737252aebf1a2a2d4814986ffb5c1184eec0a5
Gabriel melchert <gfmelchert@gmail.com>
2025-09-04 11:25:08 -0300

    feat: adiciona decorator @Public ao controlador de saúde

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
+import { Public } from 'src/shared/decorators';
 
+@Public()
 @Controller('health')
 export class HealthController {
     constructor(private readonly healthService: HealthService) { }
