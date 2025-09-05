91bb9d0a90b26db7c06937d7674679ba69db59c8
Gabriel melchert <gfmelchert@gmail.com>
2025-09-05 11:33:23 -0300

    feat: adiciona suporte a CORS para o domínio CloudFront

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
 
     app.enableCors({
-        origin: ['http://localhost:5173'],
+        origin: [
+            'http://localhost:5173',
+            "https://d1ufbj2ciwzh4u.cloudfront.net"
+        ],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
