c4ea747af6651dcbb620d772b6a45759a0faf313
Gabriel melchert <gfmelchert@gmail.com>
2025-11-07 00:04:49 -0300

    feat: atualiza versão da API para 1.1.0 no serviço de saúde

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -5,7 +5,7 @@ import { Ok } from 'src/shared/responses';
 export class HealthService {
     health() {
         return new Ok({
-            data: { version: '1.0.0' },
+            data: { version: '1.1.0' },
             message: 'API rodando!'
         })
     }
