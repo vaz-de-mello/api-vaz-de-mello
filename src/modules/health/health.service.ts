12df59f514d9aee5ae5294b36c1528297cc07b7e
Gabriel melchert <gfmelchert@gmail.com>
2025-11-18 12:00:51 -0300

    fix(health): update API version to 1.1.1

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -5,7 +5,7 @@ import { Ok } from 'src/shared/responses';
 export class HealthService {
     health() {
         return new Ok({
-            data: { version: '1.1.0' },
+            data: { version: '1.1.1' },
             message: 'API rodando!'
         })
     }
