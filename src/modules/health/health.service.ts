bcc2b967ef65e019d549799561832f7069c232e9
Gabriel melchert <gfmelchert@gmail.com>
2025-12-09 09:55:50 -0300

    change: version 1.1.5

--- src/modules/health/health.service.ts
+++ src/modules/health/health.service.ts
@@ -5,7 +5,7 @@ import { Ok } from 'src/shared/responses';
 export class HealthService {
     health() {
         return new Ok({
-            data: { version: '1.1.4' },
+            data: { version: '1.1.5' },
             message: 'API rodando!'
         })
     }
